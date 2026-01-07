ef0a26830d9a43964e1a7c2caef45051d303c9b5
hewendi23 <167489948+hewendi23@users.noreply.github.com>
2026-01-07 08:16:39 +0800

    1

--- simple-alipay-backend/src/main/java/com/example/alipay/repository/fintech/UserAuditRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.alipay.repository.fintech;
-
-import com.example.alipay.model.fintech.UserAudit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserAuditRepository extends JpaRepository<UserAudit, Long> {
-    Optional<UserAudit> findByUserId(Long userId);
-}
