01e3e0d90b4fc96a3b083f7dafbc4b9a8d656194
hewendi23 <167489948+hewendi23@users.noreply.github.com>
2026-01-10 19:53:57 +0800

    1

--- simple-alipay-backend/src/main/java/com/example/alipay/repository/CollectionQRCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.alipay.repository;
-
-import com.example.alipay.model.CollectionQRCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollectionQRCodeRepository extends JpaRepository<CollectionQRCode, Long> {
-}
